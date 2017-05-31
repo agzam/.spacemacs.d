71ec11983fc79517fa5c76985b57393bcb5a5d4f
Ag Ibragimov <agzam.ibragimov@gmail.com>
2017-05-30 22:50:49 -0700

    Org improvements

--- layers/ag-org/packages.el
+++ layers/ag-org/packages.el
@@ -28,7 +28,10 @@
         "* %u  %?\n\t%f\n\t#+BEGIN_SRC %^{language}\n\t\t%i\n\t#+END_SRC"))
 
      ort/prefix-arg-directory "~/Dropbox/org"
-     org-agenda-files '("~/Dropbox/org/" "~/Dropbox/org/pocket.org.txt")
+     org-agenda-files '("~/Dropbox/org/tasks.org"
+                        "~/Dropbox/org/yakety.org"
+                        "~/Dropbox/org/pocket.org.txt")
+
      org-refile-targets '((nil :maxlevel . 3)
                           (org-agenda-files :maxlevel . 3))
      org-refile-allow-creating-parent-nodes 'confirm
