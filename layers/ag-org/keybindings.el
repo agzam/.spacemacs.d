6ba894ef7cc2f9507e2fc1412a158797ef6d03b9
Ag Ibragimov <agzam.ibragimov@gmail.com>
2017-10-29 21:56:20 -0700

    Org-shift keybindings to H/J/K/L

--- layers/ag-org/keybindings.el
+++ layers/ag-org/keybindings.el
@@ -1,5 +1,11 @@
 (global-set-key (kbd "C-x C-p") 'org-pomodoro)
 (global-set-key (kbd "C-x p") 'org-pomodoro)
+
 (evil-define-key 'insert org-mode-map (kbd "M-RET") 'ag/org-meta-return)
 (evil-define-key 'insert org-mode-map (kbd "RET") 'org-return-indent)
 (evil-define-key 'insert org-mode-map (kbd "<S-return>") 'org-return)
+
+(evil-define-key 'normal org-mode-map "H" 'org-shiftleft)
+(evil-define-key 'normal org-mode-map "L" 'org-shiftright)
+(evil-define-key 'normal org-mode-map "J" 'org-shiftdown)
+(evil-define-key 'normal org-mode-map "K" 'org-shiftup)
