f0101bc935360bc0bfbe5a5621126560f30e32c3
Ag Ibragimov <agzam.ibragimov@gmail.com>
2017-12-16 20:42:30 -0800

    Scratch buffer is emacs-lisp by default

--- init.el
+++ init.el
@@ -164,7 +164,7 @@ It should only modify the values of Spacemacs settings."
    ;; True if the home buffer should respond to resize events. (default t)
    dotspacemacs-startup-buffer-responsive nil
    ;; Default major mode of the scratch buffer (default `text-mode')
-   dotspacemacs-scratch-mode 'org-mode
+   dotspacemacs-scratch-mode 'emacs-lisp-mode
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
