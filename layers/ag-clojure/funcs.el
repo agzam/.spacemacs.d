b0a7fb982eeb5205289cb62b67ed63310e1f69ad
Ag Ibragimov <agzam.ibragimov@gmail.com>
2017-12-16 20:43:15 -0800

    Adds `re-frame-jump-to-reg` bound to `g f`

--- layers/ag-clojure/funcs.el
+++ layers/ag-clojure/funcs.el
@@ -63,4 +63,22 @@
     (kill-new s)
     (message s)))
 
+(defun re-frame-jump-to-reg ()
+  "Borrowed from https://github.com/oliyh/re-jump.el"
+  (interactive)
+  (let* ((kw (cider-symbol-at-point 'look-back))
+         (ns-qualifier (and
+                        (string-match "^:+\\(.+\\)/.+$" kw)
+                        (match-string 1 kw)))
+         (kw-ns (if ns-qualifier
+                    (cider-resolve-alias (cider-current-ns) ns-qualifier)
+                  (cider-current-ns)))
+         (kw-to-find (concat "::" (replace-regexp-in-string "^:+\\(.+/\\)?" "" kw))))
+
+    (when (and ns-qualifier (string= kw-ns (cider-current-ns)))
+      (error "Could not resolve alias \"%s\" in %s" ns-qualifier (cider-current-ns)))
+
+    (progn (cider-find-ns "-" kw-ns)
+           (search-forward-regexp (concat "reg-[a-zA-Z-]*[ \\\n]+" kw-to-find) nil 'noerror))))
+
 ;;; funcs.el ends here
