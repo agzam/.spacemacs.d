8bccd3f8716f264884a224df2a7a71488b983736
Ag Ibragimov <agzam.ibragimov@gmail.com>
2018-05-05 06:10:24 -0700

    exwm layer

---
+++ layers/ag-exwm/config.el
@@ -0,0 +1,11 @@
+(defvar exwm--terminal-command "gnome-terminal"
+  "Terminal command to run.")
+
+(defvar exwm--locking-command "systemctl suspend"
+  "Command to run when locking session")
+
+(defvar exwm-app-launcher--prompt "$ "
+  "Prompt for the EXWM application launcher")
+
+(defvar exwm--hide-tiling-modeline nil
+  "Whether to hide modeline.")
---
+++ layers/ag-exwm/funcs.el
@@ -0,0 +1,14 @@
+;;; funcs.el --- ag-exwm layer functions file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Ag Ibragimov <agzam.ibragimov@gmail.com>
+;; URL: https://github.com/agzam/dot-spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+
+;;; funcs.el ends here
