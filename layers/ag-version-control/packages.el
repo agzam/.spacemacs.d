1b9d5db64030fb53966e6d07e4af61185f66e8e7
Ag Ibragimov <agzam.ibragimov@gmail.com>
2019-02-18 22:55:49 -0800

    magit popup actions -> transient suffixes

--- layers/ag-version-control/packages.el
+++ layers/ag-version-control/packages.el
@@ -29,49 +29,48 @@
 
   (define-key transient-map "q" #'transient-quit-all)
 
-  ;; TODO: re-write making things compatible with magit-transient
   ;; Add `git log ORIG_HEAD..HEAD` to magit
   ;; that lets you log only changes since the last pull
-  (defun magit-log-orig_head--head (&optional args files)
+  (defun magit-log-orig_head--head (args files)
     "Compare log since the last pull. i.e.: show only commits between last pull and head"
     (interactive (magit-log-arguments))
-    (magit-log (list "ORIG_HEAD..HEAD") args files))
+    (magit-git-log (list "ORIG_HEAD..HEAD") args files))
 
-  (magit-define-popup-action 'magit-log-popup
-    ?p "orig_head..head" 'magit-log-orig_head--head)
+  (transient-append-suffix 'magit-log "l"
+    '("p" "orig_head..head" magit-log-orig_head--head))
 
   (defun magit-log-other--current (revision)
     "Compare log between branches à la GitHub style.
 i.e.: show only commits that differ between selected (other branch) and current branch"
     (interactive (list (magit-read-other-branch-or-commit "Log compare")))
-    (magit-log (list (concat revision ".." (magit-get-current-branch)))))
+    (magit-git-log (list (concat revision ".." (magit-get-current-branch))) nil nil))
 
-  (magit-define-popup-action 'magit-log-popup
-    ?R "other..current" 'magit-log-other--current)
+  (transient-append-suffix 'magit-log "l"
+    '("R" "other..current" magit-log-other--current))
 
   (defun magit-log--origin-master ()
     "Compare log between branches à la GitHub style between current branch and origin/master"
     (interactive)
-    (magit-log (list (concat  "origin/master.." (magit-get-current-branch)))))
+    (magit-git-log (list (concat  "origin/master.." (magit-get-current-branch))) nil nil))
 
-  (magit-define-popup-action 'magit-log-popup
-    ?m "origin/master..current" 'magit-log--origin-master)
+  (transient-append-suffix 'magit-log "l"
+    '("m" "origin/master..current" magit-log--origin-master))
 
   (defun magit-diff-range-reversed (rev-or-range &optional args files)
     "Diff between two branches. Unlike `diff-range` works in opposite order i.e.: `base..current`"
     (interactive (list (magit-read-other-branch-or-commit "Diff range")))
-    (magit-diff (concat rev-or-range ".." (magit-get-current-branch)) args files))
+    (magit-diff-range (concat rev-or-range ".." (magit-get-current-branch)) args files))
 
-  (magit-define-popup-action 'magit-diff-popup
-    ?R "Diff range (reversed)" 'magit-diff-range-reversed)
+  (transient-append-suffix 'magit-diff "d"
+    '("R" "Diff range (reversed)" magit-diff-range-reversed))
 
   (defun magit-diff--origin-master ()
     "Compare log between branches à la GitHub style between current branch and origin/master"
     (interactive)
-    (magit-diff (concat "origin/master.." (magit-get-current-branch))))
+    (magit-diff-range (concat "origin/master.." (magit-get-current-branch))))
 
-  (magit-define-popup-action 'magit-diff-popup
-    ?m "origin/master..current" 'magit-diff--origin-master)
+  (transient-append-suffix 'magit-diff "d"
+    '("m" "origin/master..current" magit-diff--origin-master))
 
   (add-hook 'magit-hook 'turn-off-evil-mc-mode)
 
