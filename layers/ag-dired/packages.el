b693a1db8a0d34a4bd94ac6ce5bba8d3bf1fe264
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-05-07 17:42:59 -0700

    dired improvements

--- layers/ag-dired/packages.el
+++ layers/ag-dired/packages.el
@@ -9,21 +9,20 @@
 ;;
 ;;; License: GPLv3
 
-(setq ag-dired-packages '(
-                          ;; dired-rainbow
-                          ;; dired-filetype-face
-                          (direx :location (recipe :fetcher github
-                                                   :repo "agzam/direx-el"))
-                          ;; dired-quick-sort
-                          direx-grep))
-
-(if (eq system-type 'darwin)
-    (setq dired-listing-switches "-alh"
-          dired-omit-files "^\\.?#\\|^\\.DS_Store$")
-  (setq dired-listing-switches "-alh --group-directories-first"))
+(defconst ag-dired-packages '(
+                              ;; dired-rainbow
+                              ;; dired-filetype-face
+                              (direx :location (recipe :fetcher github
+                                                       :repo "agzam/direx-el"))
+                              ;; dired-quick-sort
+                              direx-grep
+                              treemacs))
 
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
-;; (add-hook 'dired-mode-hook #'dired-omit-mode)
+(setq
+ ls-lisp-dirs-first t
+ ls-lisp-use-insert-directory-program nil
+ dired-auto-revert-buffer t)
 
 (defun ag-dired/init-direx ()
   (use-package direx
@@ -51,30 +50,30 @@
           (window-resize (selected-window) 4 0 nil))))
 
     (setq direx:file-keymap
-      (let ((map (make-sparse-keymap)))
-        (define-key map "R" #'direx:do-rename-file)
-        (define-key map "C" #'direx:do-copy-files)
-        (define-key map "D" #'direx:do-delete-files)
-        (define-key map "+" #'direx:create-directory)
-        (define-key map "T" #'direx:do-touch)
-        (define-key map "j" #'direx:next-item)
-        (define-key map "J" #'direx:next-sibling-item)
-        (define-key map "k" #'direx:previous-item)
-        (define-key map "K" #'direx:previous-sibling-item)
-        (define-key map "h" #'direx:collapse-item)
-        (define-key map "H" #'direx:collapse-item-recursively)
-        (define-key map "l" #'direx:expand-item)
-        (define-key map "L" #'direx:expand-item-recursively)
-        (define-key map (kbd "RET") #'direx:maybe-find-item)
-        (define-key map "a" #'direx:find-item)
-        (define-key map "q" #'kill-this-buffer)
-        (define-key map "r" #'direx:refresh-whole-tree)
-        (define-key map "O" #'direx:find-item-other-window)
-        (define-key map "|" #'direx:fit-window)
-        (define-key map (kbd "<C-return>") #'direx:set-root)
-        (define-key map "^" #'direx:expand-root-to-parent)
-        (define-key map "o" #'spacemacs/dired-open-item-other-window-transient-state/body)
-        map))
+          (let ((map (make-sparse-keymap)))
+            (define-key map "R" #'direx:do-rename-file)
+            (define-key map "C" #'direx:do-copy-files)
+            (define-key map "D" #'direx:do-delete-files)
+            (define-key map "+" #'direx:create-directory)
+            (define-key map "T" #'direx:do-touch)
+            (define-key map "j" #'direx:next-item)
+            (define-key map "J" #'direx:next-sibling-item)
+            (define-key map "k" #'direx:previous-item)
+            (define-key map "K" #'direx:previous-sibling-item)
+            (define-key map "h" #'direx:collapse-item)
+            (define-key map "H" #'direx:collapse-item-recursively)
+            (define-key map "l" #'direx:expand-item)
+            (define-key map "L" #'direx:expand-item-recursively)
+            (define-key map (kbd "RET") #'direx:maybe-find-item)
+            (define-key map "a" #'direx:find-item)
+            (define-key map "q" #'kill-this-buffer)
+            (define-key map "r" #'direx:refresh-whole-tree)
+            (define-key map "O" #'direx:find-item-other-window)
+            (define-key map "|" #'direx:fit-window)
+            (define-key map (kbd "<C-return>") #'direx:set-root)
+            (define-key map "^" #'direx:expand-root-to-parent)
+            (define-key map "o" #'spacemacs/dired-open-item-other-window-transient-state/body)
+            map))
 
     (spacemacs/set-leader-keys-for-major-mode 'direx:direx-mode
       "o" #'direx:find-item-other-window
@@ -102,6 +101,9 @@
   (use-package dired-quick-sort
     :config
     (dired-quick-sort-setup)))
+
+(defun ag-dired/post-init-treemacs ()
+  (treemacs-resize-icons 16))
 
 ;; (defun ag-dired/init-dired-filetype-face ()
 ;;   (use-package dired-filetype-face
