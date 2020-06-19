ee196ade8b0eb39340fa8be4fd918e415d4e7513
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-06-19 16:12:31 -0700

    git-link-blame

--- layers/ag-version-control/funcs.el
+++ layers/ag-version-control/funcs.el
@@ -15,4 +15,13 @@
     (kill-new b)
     (message b)))
 
+;; remove when https://github.com/sshaw/git-link/pull/70 gets merged
+(defun git-link-blame ()
+  (interactive)
+  (cl-flet ((git-link--new* (x) (replace-regexp-in-string "/blob/" "/blame/" x)))
+    (advice-add 'git-link--new :override #'git-link--new*)
+    (let ((link (call-interactively 'git-link)))
+      (advice-remove 'git-link--new #'git-link--new*)
+      (git-link--new link))))
+
 ;;; funcs.el ends here
