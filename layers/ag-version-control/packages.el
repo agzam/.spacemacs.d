cac90b14134ae751ee4caa366b221258ef0f1997
Ag Ibragimov <agzam.ibragimov@gmail.com>
2021-02-22 22:17:41 -0600

    gh-notify + github-review goodness

--- layers/ag-version-control/packages.el
+++ layers/ag-version-control/packages.el
@@ -1,3 +1,4 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- ag-version-control layer packages file for Spacemacs.
 ;;
 ;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
@@ -11,11 +12,16 @@
 
 (defconst ag-version-control-packages '(magit
                                         gist
-                                        (github-review
-                                         :recipe (:fetcher github
-                                                           :repo "charignon/github-review"
-                                                           :files ("github-review.el")))
-                                        git-link))
+                                        (github-review :location (recipe
+                                                                  :fetcher github
+                                                                  :repo "charignon/github-review"
+                                                                  :files ("github-review.el")))
+                                        git-link
+                                        (gh-notify :location
+                                                   (recipe
+                                                    :fetcher github
+                                                    :repo "anticomputer/gh-notify"
+                                                    :branch "dev"))))
 
 (setq
  vc-follow-symlinks t
@@ -101,13 +107,28 @@ i.e.: show only commits that differ between selected (other branch) and current 
 
 (defun ag-version-control/init-github-review ()
   (use-package github-review
-    :ensure t
-    :commands magit-status-mode
-    :bind (:map magit-status-mode-map
-                ("M-r" . github-review-forge-pr-at-point)
-                :map forge-topic-mode-map
-                ("M-r" . github-review-forge-pr-at-point))
+    :after forge
     :config
+    (dolist (m (list magit-status-mode-map
+                     forge-topic-mode-map
+                     gh-notify-mode-map))
+      (evil-define-key 'normal m (kbd "M-r") 'github-review-forge-pr-at-point)
+      (define-key m (kbd "M-r") #'github-review-forge-pr-at-point))
+
+    (defun github-review--copy-suggestion ()
+      "kill a region of diff+ as a review suggestion template."
+      (interactive)
+      (setq deactivate-mark t)
+      (let ((s-region
+             (buffer-substring-no-properties
+              (region-beginning)
+              (region-end))))
+        (kill-new
+         (format "# ```suggestion\n%s\n# ```\n"
+                 (replace-regexp-in-string "^\\+" "# " s-region)))))
+
+    (evil-define-key '(visual) github-review-mode-map "y" #'github-review--copy-suggestion)
+    
     (setq github-review-fetch-top-level-and-review-comments t)))
 
 (defun ag-version-control/post-init-git-link ()
@@ -118,5 +139,44 @@ i.e.: show only commits that differ between selected (other branch) and current 
 
   (spacemacs/set-leader-keys
     "glm" #'git-link-master-branch))
+
+(defun ag-version-control/init-gh-notify ()
+  (use-package gh-notify
+    :defer t
+    :after magit
+    :init (require 'gh-notify)
+    :config
+    (evil-define-key 'normal gh-notify-mode-map (kbd "RET") 'gh-notify-visit-notification)
+    'gh-notify-forge-visit-repo-at-point
+    (spacemacs/set-leader-keys-for-major-mode 'gh-notify-mode
+      "l" #'gh-notify-retrieve-notifications
+      "r" #'gh-notify-reset-filter
+      "t" #'gh-notify-toggle-timing
+      "y" #'gh-notify-copy-url
+      "s" #'gh-notify-display-state
+      "i" #'gh-notify-ls-issues-at-point
+      "p" #'gh-notify-ls-pullreqs-at-point
+      "G" #'gh-notify-forge-refresh
+      "g" #'gh-notify-forge-visit-repo-at-point
+      "m" #'gh-notify-mark-notification
+      "M" #'gh-notify-mark-all-notifications
+      "u" #'gh-notify-unmark-notification
+      "U" #'gh-notify-unmark-all-notifications
+      "\\" #'gh-notify-toggle-url-view
+      "/d" #'gh-notify-toggle-global-ts-sort
+      "/u" #'gh-notify-limit-unread
+      "/'" #'gh-notify-limit-repo
+      "/\"" #'gh-notify-limit-repo-none
+      "/p" #'gh-notify-limit-pr
+      "/i" #'gh-notify-limit-issue
+      "/*" #'gh-notify-limit-marked
+      "/a" #'gh-notify-limit-assign
+      "/y" #'gh-notify-limit-author
+      "/m" #'gh-notify-limit-mention
+      "/t" #'gh-notify-limit-team-mention
+      "/s" #'gh-notify-limit-subscribed
+      "/c" #'gh-notify-limit-comment
+      "/r" #'gh-notify-limit-review-requested
+      "//" #'gh-notify-limit-none)))
 
 ;;; packages.el ends here
